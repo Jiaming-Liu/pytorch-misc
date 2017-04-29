3620c72f93be1cd0878643cf7f1cb56dfb111bfa
Jiaming Liu <memono.liu@gmail.com>
2017-04-29 02:03:42 -0700

    Create group_weight.py

---
+++ group_weight.py
@@ -0,0 +1,27 @@
+from torch import nn
+from torch.nn.modules.conv import _ConvNd
+from torch.nn.modules.batchnorm import _BatchNorm
+
+
+def group_weight(module):
+    group_decay = []
+    group_no_decay = []
+
+    for m in module.modules():
+        if isinstance(m, nn.Linear):
+            group_decay.append(m.weight)
+            if m.bias is not None:
+                group_no_decay.append(m.bias)
+        elif isinstance(m, _ConvNd):
+            group_decay.append(m.weight)
+            if m.bias is not None:
+                group_no_decay.append(m.bias)
+        elif isinstance(m, _BatchNorm):
+            if m.bias is not None:
+                group_no_decay.append(m.weight)
+            if m.bias is not None:
+                group_no_decay.append(m.bias)
+
+    assert len(list(module.parameters())) == len(group_decay) + len(group_no_decay)
+    groups = [dict(params=group_decay), dict(params=group_no_decay, weight_decay=.0)]
+    return groups
