f327941d6c0107ec371b0a4ab7567a1ff18a52ef
Jiaming Liu <memono.liu@gmail.com>
2017-05-01 22:45:32 -0700

    Update softmax_with_cross_entropy_loss.py

--- softmax_with_cross_entropy_loss.py
+++ softmax_with_cross_entropy_loss.py
@@ -27,9 +27,12 @@ class _SoftmaxWithCrossEntropy(Function):
             fs = torch.nn.Softmax()(
                 torch.autograd.Variable(input, requires_grad=False)
             ).data
+
+            # neg. one hot label
             y = input.new().resize_as_(input).zero_()
             for i, l in enumerate(label):
                 y[i, l] = -1.
+
             fs.add_(y).mul_(1. / len(label))
             grad_fs = fs
         if self.needs_input_grad[1]:
